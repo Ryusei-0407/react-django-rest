d47ad448e6b5f6b477e97e69a5a246c66cd2e443
Ryusei-0407 <ryu000407@gmail.com>
2021-12-04 12:25:02 +0900

    fix config

--- web/craco.config.js
+++ web/craco.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   style: {
-    postcss: [require('tailwindcss'), require('autoprefixer')],
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
   },
-};
+}
